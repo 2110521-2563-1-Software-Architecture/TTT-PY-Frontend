f9be2ff1a4e76e0b50d9b3b7adafd5a6bde5bf3f
Pluemine <pluem_ultimate@hotmail.com>
2020-10-02 23:47:52 +0700

    Add template css

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,7 +3,24 @@ import '../components/Public.css';
 function Home() {
     return (
         <div className="text-color">
-            dfsfffdfsdfdsfdsfdsfdfsfffdfsdfdsfdsfdsfdfsfffdfsdfdsfdsfdsfdfsfffdfsdfdsfdsfdsfedfddfdsfdsfdsf
+            <div className="title" style={{paddingTop:"200px",paddingLeft:"100px"}}>
+                WELCOME
+            </div>
+            <div className="box" style={{marginTop:"20px",marginBottom:"20px",marginLeft:"100px"}}>
+                Profile Name
+            </div>
+            <div className="box" style={{marginTop:"20px",marginBottom:"20px",marginLeft:"100px"}}>
+                Email
+            </div>
+            <div className="box" style={{marginTop:"20px",marginBottom:"20px",marginLeft:"100px"}}>
+                Password
+            </div>
+            <div className="box" style={{marginTop:"20px",marginBottom:"20px",marginLeft:"100px"}}>
+                Confirm Password
+            </div>
+            <div className="button" style={{marginTop:"40px",marginBottom:"20px",marginLeft:"100px"}}>
+                Register Now
+            </div>
         </div>
     )
 }
