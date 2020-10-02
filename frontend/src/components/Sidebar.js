3e94028096f540092cdcb3ea215f7e4f0e8d0e98
natcha0012 <natcha00012@gmail.com>
2020-10-02 18:10:46 +0700

    add sidebar

---
+++ frontend/src/components/Sidebar.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Sidebar.css';
+import { Link } from 'react-router-dom';
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar-container">
+        <div className="nav-menu">
+          <div className='nav-item'>
+            <Link to='/' className='nav-links'>
+              Home
+            </Link>
+          </div>
+          <div className='nav-item'>
+            <Link to='/' className='nav-links'>
+              Chat
+            </Link>
+          </div>
+          <div className='nav-item'>
+            <Link to='/' className='nav-links'>
+              Profile
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="user-profile">
+        <div>
+          <img className="user-image" src="userimage.jpeg" />
+        </div>
+        <div>
+          <div className="username">Sign In</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
