b7aa6f6bf84303ee764e9b838b4b5a0a61ac6956
natcha0012 <natcha00012@gmail.com>
2020-10-03 09:34:32 +0700

    nothing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,21 @@ import React from 'react';
 import Sidebar from './components/Sidebar';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Login from './components/pages/Login';
+import Profile from './components/pages/Profile';
+import Chat from './components/pages/Chat';
 
 function App() {
   return (
     <>
-      <Router>
-        <Sidebar/>
+      <Router >
+        <Sidebar />
         <Switch>
-          <Route path='/' exact />
+          <Route path='/' exact component={Home}/>
+          <Route path='/login' component={Login} />
+          <Route path='/profile' component={Profile} />
+          <Route path='/chat' component={Chat} />
         </Switch>
       </Router>
     </>
