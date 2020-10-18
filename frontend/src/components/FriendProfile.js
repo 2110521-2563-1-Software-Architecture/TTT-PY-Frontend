376f1d370cd39f5c4c9c060ca814a7f2973612f4
Pluemine <pluem_ultimate@hotmail.com>
2020-10-18 20:50:22 +0700

    Fixed padding and add image layout

--- frontend/src/components/FriendProfile.js
+++ frontend/src/components/FriendProfile.js
@@ -6,12 +6,13 @@ export class FriendProfile extends Component {
     render() {
         return (
             <div className="background-color">
-                <div
-                    className="title"
-                    style={{ paddingTop: "100px" }}>
+                <div style={{ paddingTop: "100px" }}>
+                    <img className="profile-image" src="userimage.jpeg"/>
+                </div>
+                <div className="profile-title">
                     Username
                 </div>
-                <div className="header-name" style={{marginBottom:'20px'}}>
+                <div className="profile-data" style={{marginBottom:'20px'}}>
                     Email : pluem@pluem.com
                 </div>
                 <div className="button" style={{marginBottom:'20px'}}>
